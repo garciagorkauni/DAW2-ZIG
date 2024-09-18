b541c2ed959c6612793cc52bef48afd4dce14545
Gorka Gacia <garcia.gorka@uni.eus>
2024-09-18 12:51:26 +0200

    Started modules second exercise

---
+++ Moduluen ariketak/02_datetime.py
@@ -0,0 +1,13 @@
+import datetime
+
+def main():
+    # Jaiotze data eskatu
+
+    # Parseatu + try-catch
+
+    # Kalkulatu urtebetetzerako egunak
+
+    # Kalkulatu jaioteguna asteko zein egun
+
+if __name__ == "__main__":
+    main()
