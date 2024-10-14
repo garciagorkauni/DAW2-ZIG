4867d5c4180ba6368ae2f5c2bc9bb53348288159
Gorka Gacia <garcia.gorka@uni.eus>
2024-10-14 08:43:14 +0200

    Develop medikua and pazientea models and migrate it

--- Azterketa-1/Azterketa/osakidetza/models.py
+++ Azterketa-1/Azterketa/osakidetza/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Pazientea(models.Model):
+    id = models.AutoField(primary_key=True)
+    izena = models.CharField(max_length=75)
+    abizena = models.CharField(max_length=75)
+    telefonoa = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.izena} {self.abizena} ({self.telefonoa})"
+
+class Medikua(models.Model):
+    id = models.AutoField(primary_key=True)
+    izena = models.CharField(max_length=75)
+    abizena = models.CharField(max_length=75)
+    espezialidadea = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.izena} {self.abizena} ({self.espezialidadea})"
